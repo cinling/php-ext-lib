989a4e19fc173dec647c8dd8c4a7efab6636ca46
Cinling <1105412681@qq.com>
2021-01-14 17:32:59 +0800

    JsonTrait 添加美化 json 的 encode

--- src/traits/JsonTrait.php
+++ src/traits/JsonTrait.php
@@ -10,11 +10,21 @@ namespace cin\extLib\traits;
 trait JsonTrait {
 
     /**
-     * @param $array
+     * @param mixed $value
+     * @param int $flags
      * @return string
      */
-    public static function encode($array) {
-        return json_encode($array, JSON_UNESCAPED_UNICODE);
+    public static function encode($value, $flags = JSON_UNESCAPED_UNICODE) {
+        return json_encode($value, $flags);
+    }
+
+    /**
+     * 美化的格式输出json
+     * @param $value
+     * @return string
+     */
+    public static function encodePretty($value) {
+        return static::encode($value, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 
     /**
